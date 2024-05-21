54a556070b7c0f91f095554a7d80671df10f6281
Jaslan12 <82884841+Jaslan12@users.noreply.github.com>
2024-05-21 18:55:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,9 @@ window.onload=()=>{
         }
     }
 }
+
+
+document.querySelector('.burger').addEventListener('click', function(){
+    this.classList.toggle('active');
+    document.querySelector('.navbar-wrap').classList.toggle('open');
+})
