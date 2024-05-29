a2bfd28be0c31222947b8f1becf4043f8169105b
Zhaslan <82884841+Jaslan12@users.noreply.github.com>
2024-05-29 18:59:49 +0500

    Add files via upload

---
+++ records/script.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.burger').addEventListener('click', function(){
+    this.classList.toggle('active');
+    document.querySelector('.navbar-wrap').classList.toggle('open');
+})
+
+
